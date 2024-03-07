a82ba7aaa027b11abfbd5bca59df1d433f0da496
verndinski <zainazayuni@gmail.com>
2024-03-07 13:52:43 +0000

    created a method to generate a dictionary representation of an instance (method to_dict()).

--- models/base_model.py
+++ models/base_model.py
@@ -10,14 +10,20 @@ class BaseModel:
     """
     Represents the model of HBNB project
     """
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """
         Initializes the BaseModel instance
         """
+	time_format = "%Y-%m-%dT%H:%M:%S.%f"
         self.id = str(uuid.uuid4())
         self.created_at = datetime.today()
         self.updated_at = datetime.today()
-
+	for len(kwargs)!= 0:
+		for key, value in kwargs.items():
+			if key == "created_at" or key == "updated_at":
+				self.__dict__[key] = datetime.strptime(value, time_format)
+	else:
+		self. __dict__[key] = value
     def save(self):
         """
         updates the public instance attribute
