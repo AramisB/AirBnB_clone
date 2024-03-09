47989b5fc84da62998a71fc19e33f6657948d8ae
AramisB <brendachem7@gmail.com>
2024-03-09 13:59:53 +0300

    A program that contains the entry point of the command interpreter

--- console.py
+++ console.py
@@ -1,7 +1,27 @@
+#!/usr/bin/python3
+"""
+This module defines the HBNBCommand
+"""
+
 import cmd
 
+
 class HBNBCommand(cmd.Cmd):
+    """
+    Represents the HBNBCommand of the HBNB project
+    """
     prompt = '(hbnb) '
 
+    def emptyline(self):
+        """Handles empty line"""
+        pass
+
     def do_quit(self, arg):
-        """Quit command
+        """Quit command to exit the program"""
+        return True
+
+    def do_EOF(self, arg):
+        """End of file command"""
+        print (" ")
+        return True
+
